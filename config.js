9789719e8f9b24a0774a7e82f40760410fab4949
RobiulHassan0 <robiulhassan270@gmail.com>
2025-09-27 14:32:16 +0600

    api working

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const SHEET_ID = "1OwgS4vpy_ojLMglQMpM1Yf5f1_SdvLa10eaxNg7VSPc"; 
-const API_KEY = "AIzaSyDbbYZX9KhChMYdpAqkWXGAtoGoDmgf3Eg"; // আপনার API Key
-const SHEET_NAME = "SiratComp2025"; // আপনার গুগল শিটের ট্যাবের নাম
+const API_KEY = "AIzaSyDbbYZX9KhChMYdpAqkWXGAtoGoDmgf3Eg"; 
+const SHEET_NAME = "Form Responses 1"; 
 const API_RANGE = 'A1:Z';
